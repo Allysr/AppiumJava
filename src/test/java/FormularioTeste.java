7f35059bcb4742f505fd064463264255053649cf
alicia souza <alicia.rsouza@gmail.com>
2024-02-06 13:34:13 -0300

    primeiro teste

--- src/test/java/FormularioTeste.java
+++ src/test/java/FormularioTeste.java
@@ -1,22 +1,42 @@
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class FormularioTeste {
 
-    @Test
-    public void deveInstalarAPK() throws MalformedURLException {
+    AndroidDriver driver;
+
+    @BeforeEach
+    public void abrirDriver() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("deviceName", "emulator-5554");
         caps.setCapability("platformName", "Android");
         caps.setCapability("automationName", "uiautomator2");
         caps.setCapability("app", "C:\\Users\\allys\\IdeaProjects\\Appium\\src\\test\\resources\\CTAppium_2_0.apk");
 
-        AndroidDriver driver = new AndroidDriver(
+        driver = new AndroidDriver(
                 new URL("http://127.0.0.1:4723"), caps
         );
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void validarFormulario(){
+        WebElement el = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Formulário' and @class='android.widget.TextView']"));
+        el.click();
+    }
+
+
+    @AfterEach
+    public void fecharDriver(){
         driver.quit();
     }
 
